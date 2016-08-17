d103d68df795287d4997ef16be24a996b8af119f
Marcus Klein <mklein@TradeMe.local>
2016-08-18 09:56:04 +1200

    added basic auth

--- server.js
+++ server.js
@@ -2,11 +2,35 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var favicon = require('serve-favicon');
+var basicAuth = require('basic-auth');
 
 var app = express();
 
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === 'kate' && user.pass === 'wanless') {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
+
 app.use(favicon(__dirname + '/dist/images/favicon.ico'));
-app.use(express.static(__dirname + "/dist"));
+app.use( '/', [ auth, express.static( __dirname + "/dist" ) ] );
+
+
 app.use(bodyParser.json());
 
 
