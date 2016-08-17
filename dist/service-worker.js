057c2c3723086a147894c82107117cc5584b9e07
Marcus Klein <mklein@TradeMe.local>
2016-08-17 14:25:21 +1200

    fixed nav bar to kates likings

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -31,7 +31,7 @@ importScripts("scripts/sw/sw-toolbox.js","scripts/sw/runtime-caching.js");
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["images/logo.png","480c70c517db1f252c8229a11443624d"],["index.html","d7aa9016d467ce4cb4eef393950cb463"],["manifest.json","30d402e280a04e6fe03b32da34c70890"],["scripts/main.min.js","34a00bde0733369f65ba9b3c8f750784"],["scripts/sw/runtime-caching.js","e3e34dcb62b5d62453b9215961585488"],["scripts/sw/sw-toolbox.js","66531e5962e4dccb0526a2b4cd6364a4"],["styles/main.css","d38d77b2bceed3243306f7501638f410"]];
+var PrecacheConfig = [["images/logo.png","480c70c517db1f252c8229a11443624d"],["index.html","eb9404a1c93b516f8ddad8df07cfa9e3"],["manifest.json","30d402e280a04e6fe03b32da34c70890"],["scripts/main.min.js","34a00bde0733369f65ba9b3c8f750784"],["scripts/sw/runtime-caching.js","e3e34dcb62b5d62453b9215961585488"],["scripts/sw/sw-toolbox.js","66531e5962e4dccb0526a2b4cd6364a4"],["styles/main.css","3fd4222a9aee1a1f009fed80715fe0b7"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1-web-starter-kit-' + (self.registration ? self.registration.scope : '') + '-';
 
